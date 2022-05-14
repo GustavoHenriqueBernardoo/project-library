f1594e43f1cde2eb7fd317b116b248ea373575c8
Gustavo Henrique Bernardo <gustavobernardo094@gmail.com>
2022-05-14 17:17:56 -0300

    feat: Get form values and store into an array

--- app.js
+++ app.js
@@ -20,25 +20,62 @@ Book.prototype.bookComplete = function (answer) {
 }
 
 function addBookToLibrary(book) {
-
+  library.push({ 'Title': book.title, 'Author': book.author, 'Pages': book.pages })
+  console.log(library)
 }
 
-const book1 = new Book('Mistborn', 'Brandon Sanderson', 380)
-book1.bookComplete('n')
+function clearAndClose() {
 
-console.log(book1)
-// console.log(book1.info())
+  // Setting the field values to nothing
+  document.getElementById('title-book').value = ''
+  document.getElementById('author-book').value = ''
+  document.getElementById('pages-book').value = ''
+
+  // Showing alert message that the books was added
+  setTimeout(function () { document.querySelector('.message-add').hidden = false }, 3000)
+}
+
 
 const modal = document.querySelector('.modal')
-console.log(modal)
+
+// Open the modal
 document.querySelector('.addBook').addEventListener('click', () => {
   modal.style.opacity = 1
-  modal.style.zIndex = 1
-
+  modal.style.zIndex = 999
 })
 
+// Close the modal
 const closeBtn = document.querySelector('.btnClose').addEventListener('click', () => {
   modal.style.opacity = 0
   modal.style.zIndex = -1
-
 })
+
+// const submitBtn = document.querySelector('.submit').addEventListener('click', (e) => {
+//   e.preventDefault()
+//   const title = document.getElementById('title-book').value
+//   const author = document.getElementById('author-book').value
+//   const pages = document.getElementById('pages-book').value
+//   const newBook = new Book(title, author, pages)
+//   addBookToLibrary(newBook)
+// })
+
+const bookForm = document.querySelector('[name="book-form"]').addEventListener('submit', (e) => {
+
+  // Get form values
+  const title = e.currentTarget.title.value
+  const author = e.currentTarget.author.value
+  const pages = e.currentTarget.pages.value
+
+  // Instantiating a new book
+  const newBook = new Book(title, author, pages)
+
+  // Adding the new book 
+  addBookToLibrary(newBook)
+
+  // Cleaning the fields
+  clearAndClose()
+
+  // Prevent the default behavior of the submit button
+  e.preventDefault()
+})
+console.log(bookForm)
